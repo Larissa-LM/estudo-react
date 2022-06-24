14ddf20f07559772446da1956d8f43c0ae6d9a35
Larissa-LM <larissa.maria@creditas.com>
2022-06-24 16:20:43 -0300

    Aprendendo state

--- react-tutorial-tania/src/Components/Table.js
+++ react-tutorial-tania/src/Components/Table.js
@@ -19,6 +19,9 @@ const TableBody = (props) => {
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td>
+                    <button onClick={()=> props.removeCharacter(index)}>Delete</button>
+                </td>
             </tr>
         )
 
@@ -27,17 +30,16 @@ const TableBody = (props) => {
      return <tbody>{rows}</tbody>            
 }
 
-class Table extends Component {
-    render(){
-        const {characterData} = this.props
+const Table = (props) => {
+    const {characterData,removeCharacter} = props
         return (
             <table>
                <TableHeader/>
-               <TableBody characterData={characterData}/>
+               <TableBody characterData={characterData} removeCharacter = {removeCharacter}/>
                 
             </table>
         )
-    }
+    
 }
 
 export default Table
